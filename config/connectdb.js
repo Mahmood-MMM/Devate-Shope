92657cb7c0d55ea1cf0134f18c75f24e1f14f8a1
Mahmood Osman <mahmoodna3n3@gmail.com>
2023-07-22 13:39:33 +0300

    Initail commit

--- config/connectdb.js
+++ config/connectdb.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 const mongoose = require('mongoose')
 
 const connectDB = () => {
-mongoose.connect('mongodb+srv://Ecommerce:mahmood0100794@ecommerce.zxqtdef.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+  mongoose.connect('mongodb+srv://mahmoodna3n3:M02ysvvKd2XLdSiw@cluster0.zlvnvgh.mongodb.net/?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, err =>{
-    if(err) throw err;
-    console.log('Connected to MongoDB')
-})
+  }, err = {
+    if(err) {
+      throw err
+      console.log('Connected to MongoDB')
+    }
+  })
 }
 
 module.exports = connectDB
