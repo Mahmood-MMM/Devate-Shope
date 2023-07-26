64d2ce1774fc9037ad4942b579747d2142ce9106
Mahmood-MMM <109480929+Mahmood-MMM@users.noreply.github.com>
2023-07-27 01:30:04 +0300

    update

--- config/connectdb.js
+++ config/connectdb.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require('mongoose')
 
 const connectDB = () => {
-  mongoose.connect("process.env.URI", {
+  mongoose.connect(process.env.URI, {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
